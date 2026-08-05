8794afe2ad3b5901e1626cf5a5796b3bb03d9901
Dustin Henry Offutt <dusty@tabith.com>
2026-08-05 14:13:08 -0600

    export MemfdStrategy and create_strategy from strategies.__init__

--- packages/executor/src/executor/strategies/__init__.py
+++ packages/executor/src/executor/strategies/__init__.py
@@ -1,5 +1,7 @@
 """Injection strategy modules."""
 
 from .base import InjectionResult, InjectionStrategy
+from .factory import create_strategy
+from .memfd_strategy import MemfdStrategy
 
-__all__ = ["InjectionResult", "InjectionStrategy"]
+__all__ = ["InjectionResult", "InjectionStrategy", "MemfdStrategy", "create_strategy"]
