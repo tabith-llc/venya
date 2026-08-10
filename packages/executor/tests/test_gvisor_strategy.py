b2a3ee831683b38a41cbd7305c86b54f034dcd66
Dustin Henry Offutt <dusty@tabith.com>
2026-08-09 18:05:29 -0600

    test(executor): add GvisorStrategy.name() test

---
+++ packages/executor/tests/test_gvisor_strategy.py
@@ -0,0 +1,12 @@
+"""Tests for GvisorStrategy."""
+
+from executor.strategies.gvisor_strategy import GvisorStrategy
+
+
+class TestGvisorStrategyName:
+    """Tests for GvisorStrategy.name()."""
+
+    def test_returns_gvisor(self):
+        """name() returns 'gvisor'."""
+        strategy = GvisorStrategy()
+        assert strategy.name() == "gvisor"
