f3bbc783cc4d95d5e9b4ddecedca82c79b56d1cd
Dustin Henry Offutt <dusty@tabith.com>
2026-08-09 18:05:50 -0600

    test(executor): add GvisorStrategy init tests

--- packages/executor/tests/test_gvisor_strategy.py
+++ packages/executor/tests/test_gvisor_strategy.py
@@ -10,3 +10,22 @@ class TestGvisorStrategyName:
         """name() returns 'gvisor'."""
         strategy = GvisorStrategy()
         assert strategy.name() == "gvisor"
+
+
+class TestGvisorStrategyInit:
+    """Tests for GvisorStrategy initialization."""
+
+    def test_default_secret_base_fd(self):
+        """secret_base_fd defaults to 100."""
+        strategy = GvisorStrategy()
+        assert strategy.secret_base_fd == 100
+
+    def test_session_dir_is_none_initially(self):
+        """_session_dir is None before prepare() is called."""
+        strategy = GvisorStrategy()
+        assert strategy._session_dir is None
+
+    def test_custom_secret_base_fd(self):
+        """secret_base_fd can be set to a custom value."""
+        strategy = GvisorStrategy(secret_base_fd=200)
+        assert strategy.secret_base_fd == 200
